d71cf69c9180c0097b574621c31975e2dd65f8d6
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-09-14 05:04:29 +0200

    Update Maevement_LumaHunt_Disconnect.lua

--- Maevement_LumaHunt_Disconnect.lua
+++ Maevement_LumaHunt_Disconnect.lua
@@ -34,10 +34,7 @@ if tblua:IsInWorld() == true then
            tblua:CircleArea()
            local xd = math.random(100, 300)
            tblua:Sleep(xd)
-           local mhyes = math.random(2)
-           if mhyes == 1 then
            MovementSwitch = 2
-           end
         elseif MovementSwitch == 2 then
            tblua:RandomArea()
            local xdd = math.random(400, 1200)
